3152190b12eea778d60c16bfe38e52900e7b89cb
Amrutaquickitdotnet <velankaramruta@gmail.com>
2023-08-11 19:53:38 +0530

    Added ReadExcel

---
+++ ReadExcelWithXSSF/src/test/java/com/excel/TestExecution.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+public class TestExecution {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		ReadData rd = new ReadData();
+		rd.readexcel();
+
+	}
+
+}
